dae6d40e7e484792c77d26c48c99e4cf9e49381b
Jeremy Su <70295411+bulletchicken@users.noreply.github.com>
2023-05-25 10:27:39 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,27 @@
-import java.util.*;
 
+import java.util.*;
 public class Main{
-    public static void main(String[]args){
-        generateOutcomes();
-        computerGuess();
-    }
     private static final char[] COLORS = {'G', 'R', 'B', 'Y', 'O', 'P'};
     private static ArrayList<ArrayList<Character>> possibleCombinations = new ArrayList<>();
+    private static ArrayList<Character>code = new ArrayList<Character>();
+    public static void main(String[]args){
+    	ArrayList<Character> guess = new ArrayList<Character>();
+        generateOutcomes();
+        computerGuess();
+        code.add('G');
+        code.add('G');
+        code.add('Y');
+        code.add('Y');
+        guess.add('G');
+        guess.add('G');
+        guess.add('G');
+        guess.add('Y');
+        int results [] = check(guess);
+        for(int i = 0; i < results.length; i++){
+        	System.out.println(results[i]);
+        }
+    }
+
     public static void generateOutcomes(){
 
         
@@ -27,11 +42,20 @@ public class Main{
     }
 
     public static int[] check(ArrayList<Character>guess){
-        
+    	int[]results = new int[2];
+		for(int i = 0; i < guess.size(); i++){
+			if(code.get(i) == guess.get(i)){
+				results[0]++;
+			}
+			else if(code.contains(guess.get(i))){
+				results[1]++;
+			}
+		}
+		return results;
     }
 
     public static void computerGuess(){
-
+    	char bond[] = new char[2];
     }
 
     
