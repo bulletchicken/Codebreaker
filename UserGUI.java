75a31379668c252288f43e694177219e1f5fb59a
Eric Chen <70295495+cloudec11@users.noreply.github.com>
2023-05-27 20:27:43 -0400

    Update UserGUI.java

--- UserGUI.java
+++ UserGUI.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class UserGUI extends JFrame implements ActionListener{
