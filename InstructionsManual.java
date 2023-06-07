2845deebf2c243e33aa8c35fe1645e918c52f96e
Jeremy Su <70295411+bulletchicken@users.noreply.github.com>
2023-06-07 01:24:22 -0400

    Update InstructionsManual.java

--- InstructionsManual.java
+++ InstructionsManual.java
@@ -19,16 +19,18 @@ public class InstructionsManual extends JFrame{
     JLabel howToComputerGuessing = new JLabel("<html> <br> Instructions for Computer Guessing</html>");
 
     JTextArea howToPlayText = new JTextArea("\n\n1. A 4 long code is made chosen from 6 colours (Red, Green, Blue, Yellow, Pink, Orange). \n\n2. The other player has to guess the code by also creating a 4 long code from the 6 colours within 10 moves \n\n3. Every guess the player takes, they receive hints in forms of black and white pins. For every colour the guess matches in the correct slot to the answer, they recieve a black pin on the side. For every colour that matches but is in the wrong slot recieves a white pin on the side. ");
-    JTextArea howToUserGuessingText = new JTextArea("\nWhen you load up the mode, a randomized code is set and you can begin guessing right away. \n\nHow do I guess?\nTo create your guess, you first click the colour you want to place from the vertical column of colours. Then click the circle you want to colour in one of the slots highlighted. Afterwards, you can press submit to finalize your answer and recieve hints. \n\nHow can I change my guess? \nIf you want to change your guess before you submit, you can reclick your slot with your chosen colour, or erasing your current guess by clicking the clear button.\n\nHow can I restart the game?\nYou can restart the game simply by clicking the restart button!");
-    JTextArea howToComputerGuessingText = new JTextArea("\nWhen you load up the mode, you will need to create a secret code for the computer to guess. \n\nHow Do I create a code?\nTo create a code, you first select the colour you want from the side bar, then click a slot in one of the highlighted squares. Once you have created a guess, press submit and the computer will begin to guess.\n\nHow do I give hints?\nAfter the computer has guessed, you have to give it hints in forms of black and white pegs (look at How to Play Codebreaker instructions on how black and white hints work). There will be a box... ");
+    JTextArea howToUserGuessingText = new JTextArea("\nWhen you load up the mode, a randomized code is set and you can begin guessing right away. \n\nHow do I guess?\nTo create your guess, you first click the colour you want to place from the selection of colours column. Then click one of the slots highlighted in white to the left to colour that section of the guess. Repeat until you have a full guess of 4 colours. Afterwards, you can press submit to finalize your answer and recieve hints. \n\nHow can I change my guess? \nIf you want to change your guess before you submit, you can reclick your slot with your chosen colour, or erasing your current guess by clicking the clear button.\n\nHow can I restart the game?\nYou can restart the game simply by clicking the restart button!");
+    JTextArea howToComputerGuessingText = new JTextArea("\nWhen you load up the mode, you will need to think of and remember a secret code for the computer to guess(we recommend typing out the colours on notepad to help memorize).\n\nRequirements for the code?\nThe code should be four colours long using the set of 6 colours on the bottom.\n\nHow do I give hints?\nAfter the computer has guessed, you have to give it hints in forms of black and white pegs (look at How to Play Codebreaker instructions on how black and white hints work). Beside the guesses, there will be a 2x2 grid. These are where the hints can go. You can select either black or white from the left side of the guesses, then click the hints to place them. If you want to erase a hint, you can click clear in the bottom.");
 
     public InstructionsManual() {
         //adjusting the main window's settings
         setTitle("Code Breaker - Instruction Manual");
-        setSize(1200, 800);
+        setSize(1200, 900);
         setResizable(true);
         setLayout(new BorderLayout());
         getContentPane().setBackground(Color.WHITE);
+        
+
     
         //setting the main grid layout
         mainUI.setLayout(new GridLayout(1, 3, 20, 0));
